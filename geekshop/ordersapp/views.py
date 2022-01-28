9a8d6e727f217513e361d49c703a1be03065b011
rxfix <panter2004@gmail.com>
2022-01-28 14:28:57 +0300

    orders.html

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from ordersapp.models import Order
+
+
+class OrderList(ListView):
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
