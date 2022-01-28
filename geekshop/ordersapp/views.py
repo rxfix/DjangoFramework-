122908943f1206894a38fb482797cdd5d552b8ac
rxfix <panter2004@gmail.com>
2022-01-28 15:10:32 +0300

    OrderUpdate

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
@@ -59,3 +59,41 @@ class OrderCreate(CreateView):
             self.object.delete()
 
         return super(OrderCreate, self).form_valid(form)
+
+
+
+class OrderUpdate(UpdateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('orders:orders_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderUpdate, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
+        context['orderitems'] = formset
+
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        if self.object.get_total_cost() == 0:
+            self.object.delete()
+
+        return super(OrderUpdate, self).form_valid(form)
