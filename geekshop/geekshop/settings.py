36443512e872aaafbc775c997897da2c7e49359e
rxfix <panter2004@gmail.com>
2022-02-04 16:03:56 +0300

    STATIC_ROOT

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -146,9 +146,10 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "geekshop", "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "geekshop", "static"),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
